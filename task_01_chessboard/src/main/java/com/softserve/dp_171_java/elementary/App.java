6602d1b7d306bb147a190bdf11933f970f20bf28
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-05 15:21:11 +0300

    some fixes "ChessboardImpl.java" in "task_01_chessboard" via branch "task_01_chessboard"

--- task_01_chessboard/src/main/java/com/softserve/dp_171_java/elementary/App.java
+++ task_01_chessboard/src/main/java/com/softserve/dp_171_java/elementary/App.java
@@ -12,12 +12,18 @@ public class App {
             int height = getArgsElement(args, 0);
             int width = getArgsElement(args, 1);
 
+            if ((height <= 0) && (width <=0)) {
+                System.out.println("Entering arguments have to been more than zero." +
+                        " Try again with using positive number, please");
+                return;
+            }
+
             Chessboard chessboard = new ChessboardImpl(height, width);
             chessboard.createChessboard();
 
         } catch (NumberFormatException ex) {
 
-            System.out.println("Arguments entered in the wrong format." +
+            System.out.println("Arguments were entered in the wrong format." +
                     " Try again with using integer digits, please.");
 
         } catch (ArrayIndexOutOfBoundsException ex) {

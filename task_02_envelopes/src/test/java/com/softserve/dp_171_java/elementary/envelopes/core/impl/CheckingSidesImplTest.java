98c8fc7632863a2a4c15019f2a4094c8fcd96877
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-07 15:02:28 +0300

    added some tests to "task_02_envelopes"

--- task_02_envelopes/src/test/java/com/softserve/dp_171_java/elementary/envelopes/core/impl/CheckingSidesImplTest.java
+++ task_02_envelopes/src/test/java/com/softserve/dp_171_java/elementary/envelopes/core/impl/CheckingSidesImplTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class CheckingSidesImplTest {
 
     @Test
-    public void checkSidesTrueTest() {
+    public void checkSidesFirstEnvLessSecEnvTrueTest() {
         //GIVEN
         CheckingSidesImpl checkingSides = new CheckingSidesImpl();
         //WHEN
@@ -18,6 +18,61 @@ public class CheckingSidesImplTest {
                 2.0,
                 4.0,
                 5.0));
+    }
+
+    @Test
+    public void checkSidesSecEnvLessFirstEnvTrueTest() {
+        //GIVEN
+        CheckingSidesImpl checkingSides = new CheckingSidesImpl();
+        //WHEN
+        //THEN
+        assertTrue(checkingSides.checkSides(5.0,
+                4.0,
+                1.0,
+                2.0));
+    }
+
+
+    @Test
+    public void checkSidesTrueEqualsTest() {
+        //GIVEN
+        CheckingSidesImpl checkingSides = new CheckingSidesImpl();
+        boolean expected = true;
+        //WHEN
+        boolean actual = checkingSides.checkSides(1.0,
+                2.0,
+                4.0,
+                5.0);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkSidesFalseEqualsTest() {
+        //GIVEN
+        CheckingSidesImpl checkingSides = new CheckingSidesImpl();
+        boolean expected = false;
+        //WHEN
+        boolean actual = checkingSides.checkSides(1.0,
+                1.0,
+                1.0,
+                1.0);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkSidesNotEqualsTest() {
+        //GIVEN
+        CheckingSidesImpl checkingSides = new CheckingSidesImpl();
+        boolean unexpected = true;
+        //WHEN
+        boolean actual = checkingSides.checkSides(1.0,
+                1.0,
+                -1.0,
+                -1.0);
+        //THEN
+        assertNotEquals(unexpected, actual);
     }
 
     @Test
@@ -31,5 +86,31 @@ public class CheckingSidesImplTest {
                 2.0,
                 1.0,
                 2.0));
+    }
+
+    @Test
+    public void checkSidesSecEnvLessFirstEnvFalseTest() {
+        //GIVEN
+        CheckingSides checkingSides = new CheckingSidesImpl();
+        //WHEN
+        //THEN
+        assertFalse(checkingSides.checkSides(
+                5.0,
+                4.0,
+                4.0,
+                4.0));
+    }
+
+    @Test
+    public void checkSidesFirstEnvelopeLessSecondEnvelopeFalseTest() {
+        //GIVEN
+        CheckingSides checkingSides = new CheckingSidesImpl();
+        //WHEN
+        //THEN
+        assertFalse(checkingSides.checkSides(
+                4.0,
+                4.0,
+                5.0,
+                4.0));
     }
 }

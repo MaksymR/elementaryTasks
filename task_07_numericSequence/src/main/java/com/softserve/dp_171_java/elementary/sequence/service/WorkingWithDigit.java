fbde6661b0b0d725bd1f7694c77cc3373d98d62a
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-08 12:21:21 +0300

    added some comments to "WorkingWithDigit.java" of "task_07_numericSequence"

--- task_07_numericSequence/src/main/java/com/softserve/dp_171_java/elementary/sequence/service/WorkingWithDigit.java
+++ task_07_numericSequence/src/main/java/com/softserve/dp_171_java/elementary/sequence/service/WorkingWithDigit.java
@@ -8,10 +8,17 @@ public class WorkingWithDigit {
     private final static int EXPONENT = 2;
 
     public Map<Integer, Integer> workWithDigit(int digit) {
+
+        Map<Integer, Integer> resultList = new HashMap<>();
+
         if (digit <= 0) {
             System.out.println("\nDon't enter a negative digit, please");
         }
-        Map<Integer, Integer> resultList = new HashMap<>();
+
+        /*
+        If a "digit" (from argument) is zero or negative number then next cycle "for" does not work,
+        because condition "i < digit" (e.g. -> "1 < 0" or "1 < -1") is false.
+        */
         for (int i = 1; i < digit; i++) {
             int tempValue = (int) Math.pow(i, EXPONENT);
             if (tempValue < digit) {
@@ -20,7 +27,9 @@ public class WorkingWithDigit {
                 break;
             }
         }
+
         return resultList;
+
     }
 
 }
